44b1e7667c03fb3ef503c518697a865bc41421d4
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-08-20 21:28:41 +0400

    selectedpage

--- client/src/layout/selectedPage.jsx
+++ client/src/layout/selectedPage.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 // import UserProvider from "../hooks/useUsers";
 import { useFavorite } from "../hooks/useFavorite";
-import UseFetch from "../hooks/useFetch";
+// import UseFetch from "../hooks/useFetch";
 import { API_URL, API_KEY } from "../utils/openWeatherInfo";
 import ItemCardFavorite from "../page/itemCardFavorite";
 
 const SelectedPage = () => {
     const { getFavorites } = useFavorite();
-    const { data, setUrl } = UseFetch();
+    // const { data, setUrl } = UseFetch();
     const [favorite, setFavorites] = useState([]);
+    const arr = [];
     useEffect(() => {
         const fetchFavorite = async () => {
             const result = await getFavorites();
@@ -17,13 +18,23 @@ const SelectedPage = () => {
         fetchFavorite();
     }, []);
     console.log(favorite);
-    return (
-        <>
-            {/* <UserProvider> */}
-            {
-                (favorite.map((item) => {
-                    setUrl(`${API_URL}weather?id=${item.cityId}&appid=${API_KEY}&units=metric`);
-                    console.log(data);
+    const requests = favorite.map((item) => fetch(`${API_URL}weather?id=${item.cityId}&appid=${API_KEY}&units=metric`));
+    Promise.all(requests)
+        .then((responses) => {
+            const dataResults = responses.map((response) => response.json());
+            console.log(dataResults);
+            return Promise.all(dataResults);
+        })
+        .then((cities) => {
+            cities.forEach((data) => {
+                console.log(data);
+                arr.push(data);
+            });
+        });
+        console.log(arr)
+        return (
+            <>
+                (arr.map((data) => {
                     return (
                         <ItemCardFavorite key={data.id}
                             dt={data.dt}
@@ -36,12 +47,35 @@ const SelectedPage = () => {
                             data={data}
                             id={data.id}
                         />
-                    );
+                    )
                 }))
-            }
-            {/* </UserProvider> */}
-        </>
-    );
+            </>
+        )
+    // return (
+    //     <>
+    //         {/* <UserProvider> */}
+    //         {
+    //             (cities.map((data) => {
+    //                 {/* setUrl(`${API_URL}weather?id=${item.cityId}&appid=${API_KEY}&units=metric`);
+    //                 console.log(data); */}
+    //                 return (
+                        // <ItemCardFavorite key={data.id}
+                        //     dt={data.dt}
+                        //     tempMin={data.main.temp_min}
+                        //     tempMax={data.main.temp_max}
+                        //     name={data.name}
+                        //     country={data.sys.country}
+                        //     icon={data.weather[0].icon}
+                        //     wind={data.wind.speed}
+                        //     data={data}
+                        //     id={data.id}
+                        // />
+    //                 );
+    //             }))
+    //         }
+    //         {/* </UserProvider> */}
+    //     </>
+    // );
 };
 
 export default SelectedPage;
